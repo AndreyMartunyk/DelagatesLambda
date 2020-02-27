ed256d224f4e291bf79f6ec62cb41625db50e19b
Andrey Martunyk <aniks.zoom@gmail.com>
2020-02-27 15:00:10 +0200

    done

---
+++ DelagatesLambda/MyMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelagatesLambda
+{
+    class MyMath
+    {
+        // я тут немного поигрался с разными вариациями лямбда выражений
+
+        public delegate double MathDelegate(double a, double b);
+
+        //public MathDelegate Add = (double a, double b) => a + b;
+        public MathDelegate Add // 4) вот тут я немног поигрался, мне стало интересто можно ли сделать скрытый делегат от подписки
+        {                       // оказалось можно, но я так и не придумал зачем?))
+            get                 // хотя если представить наши делегаты лишь набор действий/свойств присущие обьекту
+            {                   // и мы складываем и выкладывам их из контейнера этих делегатов при опред. условиях, меняя его поведение
+                return (double a, double b) => a + b;  //так делают? или бред какой-то?)
+            }
+            private set { }
+        }
+        public double Sub(double a, double b) => a - b; // 1) тот случай когда лябда выражение просто "укорачивает" запись, никакого отношения к делегатам тут нет.
+        public MathDelegate Mul = (double a, double b) => a * b; // 2) самый тривиальный случай исспользования лямбды
+        public MathDelegate Div = (double a, double b) => // 3) тут пришлось прибегнуть к скобкам, так как не получается сделать все в одну строчку*
+        {                                                 // *вообще можно было бы исспользовать тернарный оператор, но я читал что это так себе практика и лучше ею не пользоваться, да и мне не очень нравится. Одно дело когда это пишу я, другое когда читаю чужой тернарник
+            if (a == 0)
+            {
+                return 0;
+            }
+
+            return a / b;
+        };
+    }
+}
--- DelagatesLambda/Program.cs
+++ DelagatesLambda/Program.cs
@@ -8,8 +8,61 @@ namespace DelagatesLambda
 {
     class Program
     {
+        delegate double MyDelegate (double a, double b, double c);
+        delegate int Rand();
+        delegate double ArrDelegate(Rand[] r);
+        static Random random = new Random();
+
         static void Main(string[] args)
         {
+            #region Task 1 
+            // 1)  Создайте анонимный метод, который принимает в качестве параметров три целочисленных аргумента и
+            //возвращает среднее арифметическое этих аргументов.
+
+            MyDelegate average = delegate (double a, double b, double c) { return (a + b + c) / 3.0; }; // просто анонимный метод
+            average = (double a, double b, double c) => (a + b + c) / 3.0; //  и с исспользованием лямбда выражений
+            #endregion
+
+            #region Task 2
+            // 2) Создайте четыре лямбда оператора для выполнения арифметических действий: (Add – сложение, Sub –
+            //вычитание, Mul – умножение, Div – деление). Каждый лямбда оператор должен принимать два аргумента и
+            //возвращать результат вычисления.Лямбда оператор деления должен делать проверку деления на ноль.
+            //Написать программу, которая будет выполнять арифметические действия указанные пользователем.
+            MyMath myMath = new MyMath();
+            myMath.Add(2,3);
+            myMath.Sub(2, 2);
+            #endregion
+
+            #region Task 3
+            // 3) Создайте анонимный метод, который принимает в качестве аргумента массив делегатов и возвращает среднее
+            //арифметическое возвращаемых значений методов сообщенных с делегатами в массиве.Методы, сообщенные с
+            //делегатами из массива, возвращают случайное значение типа int.
+
+           
+            Rand[] rands = new Rand[10];
+            for (int i = 0; i < rands.Length; i++)
+            {
+                rands[i] = () => random.Next(1,100);
+                Console.WriteLine(rands[i]());
+            }
+
+            Console.WriteLine("Average = " + AverageIt(rands) );
+
+            #endregion
+
+
+            Console.ReadKey();
         }
+
+        static ArrDelegate AverageIt = (Rand[] dels) =>
+        {
+            int sum = 0;
+            foreach (Rand i in dels)
+            {
+                sum += i();
+            }
+
+            return sum / dels.Length;
+        };
     }
 }
